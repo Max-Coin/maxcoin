f5fe7f68eabf0bd4855583eb9e008d8bdf0ed6f0
Luke <lukem512@users.noreply.github.com>
2014-03-20 11:55:44 +0000

    Added seed node URL

--- src/net.cpp
+++ src/net.cpp
@@ -1191,6 +1191,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"maxcoin.co.uk", "dnsseed.maxcoin.co.uk"},
     {NULL, NULL}
 };
 
