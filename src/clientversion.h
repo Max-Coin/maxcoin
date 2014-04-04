a7e4950c438039a5e595e9edaf97b344cb699af1
Luke <lukem512@users.noreply.github.com>
2014-04-04 16:58:59 +0100

    HARD FORK @ 167500

--- src/clientversion.h
+++ src/clientversion.h
@@ -5,10 +5,10 @@
 // client versioning and copyright year
 //
 
-// These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
+// These need to be macros, as version.cpp's and maxcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
-#define CLIENT_VERSION_REVISION    0
+#define CLIENT_VERSION_REVISION    1
 #define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
