6978530810df3fb8ffed2f662c24a07d5d4b9de2
Luke <lukem95@gmail.com>
2014-09-16 20:46:21 +0100

    Checkpoint before rogue block

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,11 +39,12 @@ namespace Checkpoints
         ( 25000, uint256("0x000000000000892fe49518331d3ce99075a61ae03fe0c3fb5363babf793f9ed5"))
         (111111, uint256("0x0000000000023b44c09a7f8740cec05de8d88e7cbc606457cf86c45a8f1c2c1d"))
         (557625, uint256("0x0000000000057faf782713f21104bffcfc8cc9107df88dea399b06ffe3e0d6c4"))
+        (630899, uint256("0x0000000000049887343d4bff3be773dafd7980d681c076216fe79fc1131d4068"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1408634353, // * UNIX timestamp of last checkpoint block
-        0,          // * total number of transactions between genesis and last checkpoint
+        1410884581, // * UNIX timestamp of last checkpoint block
+        1684532,          // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         1000.0      // * estimated number of transactions per day after checkpoint
     };
