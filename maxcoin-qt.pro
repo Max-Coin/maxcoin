65c8738aa43ea62720aa97d7c30aa5aba207d3c3
goombaloon <goombaloon@gmail.com>
2017-08-24 23:06:51 -0400

    Incremented version number to 0.9.4 and updated link to primary Maxcoin website.

--- maxcoin-qt.pro
+++ maxcoin-qt.pro
@@ -1,7 +1,7 @@
 TEMPLATE = app
 TARGET = maxcoin-qt
 macx:TARGET = "MaxCoin-Qt"
-VERSION = 0.9.3.1
+VERSION = 0.9.4.0
 INCLUDEPATH += src src/json src/qt
 QT += network
 DEFINES += QT_GUI BOOST_THREAD_USE_LIB BOOST_SPIRIT_THREADSAFE
--- src/clientversion.h
+++ src/clientversion.h
@@ -8,15 +8,15 @@
 // These need to be macros, as version.cpp's and maxcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
-#define CLIENT_VERSION_REVISION    3
-#define CLIENT_VERSION_BUILD       1
+#define CLIENT_VERSION_REVISION    4
+#define CLIENT_VERSION_BUILD       0
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
-#define COPYRIGHT_YEAR 2014
+#define COPYRIGHT_YEAR 2017
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
