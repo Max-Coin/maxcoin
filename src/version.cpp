7c4d36c6f17779706c8962189e7b007238322f53
goombaloon <goombaloon@gmail.com>
2017-08-26 12:40:40 -0400

    Reverted GIT_COMMIT_ID back to previous approach.

--- src/version.cpp
+++ src/version.cpp
@@ -33,9 +33,10 @@ const std::string CLIENT_NAME("Max");
 #    include "build.h"
 #endif
 
-// git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+// git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
+#define GIT_ARCHIVE 0 
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "$Format:%h$"
+#    define GIT_COMMIT_ID "8c5e74e"
 #    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
